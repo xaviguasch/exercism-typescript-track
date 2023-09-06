41f03224596faff854116ff387fac68ac0aee311
Xavi Guasch <xaviguasch@gmail.com>
2023-09-06 23:03:18 +0200

    added 017-clock exercise

---
+++ 017-clock/clock.ts
@@ -0,0 +1,28 @@
+// Instructions
+// Implement a clock that handles times without dates.
+
+// You should be able to add and subtract minutes to it.
+
+// Two clocks that represent the same time should be equal to each other.
+
+export class Clock {
+  constructor(hour: unknown, minute?: unknown) {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  public toString(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  public plus(minutes: unknown): Clock {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  public minus(minutes: unknown): Clock {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  public equals(other: unknown): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+}
