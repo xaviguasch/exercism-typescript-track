c5a0b92f1ab7ec514c1c8c7535a7424dd0a17405
Xavi Guasch <xaviguasch@gmail.com>
2023-09-29 22:22:29 +0200

    added 026-reverse-string challenge

---
+++ 026-reverse-string/reverse-string.ts
@@ -0,0 +1,8 @@
+// Instructions
+// Reverse a string
+
+// For example: input: "cool" output: "looc"
+
+export function reverse(/* Parameters go here */) {
+  throw new Error('Remove this statement and implement this function')
+}
