cfd7ccda31caf1bed609522664075204ddeeae1f
Xavi Guasch <xaviguasch@gmail.com>
2023-11-07 23:00:15 +0100

    added 056-rectangles challenge

---
+++ 056-rectangles/rectangles.ts
@@ -0,0 +1,38 @@
+// Instructions
+// Count the rectangles in an ASCII diagram like the one below.
+
+//    +--+
+//   ++  |
+// +-++--+
+// |  |  |
+// +--+--+
+// The above diagram contains these 6 rectangles:
+
+// +-----+
+// |     |
+// +-----+
+//    +--+
+//    |  |
+//    |  |
+//    |  |
+//    +--+
+//    +--+
+//    |  |
+//    +--+
+
+//    +--+
+//    |  |
+//    +--+
+
+// +--+
+// |  |
+// +--+
+
+//   ++
+//   ++
+
+// You may assume that the input is always a proper rectangle (i.e. the length of every line equals the length of the first line).
+
+export function count() {
+  throw new Error('Remove this statement and implement this function')
+}
