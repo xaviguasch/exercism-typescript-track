c09156700d3b45064d9f3cfcf2d9281d895a44f4
Xavi Guasch <xaviguasch@gmail.com>
2023-11-09 23:08:52 +0100

    added 058-custom-set challenge

---
+++ 058-custom-set/custom-set.ts
@@ -0,0 +1,46 @@
+// Instructions
+// Create a custom set type.
+
+// Sometimes it is necessary to define a custom data structure of some type, like a set. In this exercise you will define your own set. How it works internally doesn't matter, as long as it behaves like a set of unique elements.
+
+export class CustomSet {
+  constructor(initial?: unknown) {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  empty(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  contains(element: unknown): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  add(element: unknown): CustomSet {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  subset(other: unknown): CustomSet {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  disjoint(other: unknown): CustomSet {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  eql(other: unknown): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  union(other: unknown): CustomSet {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  intersection(other: unknown): CustomSet {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  difference(other: unknown): CustomSet {
+    throw new Error('Remove this statement and implement this function')
+  }
+}
